1a586f2f5e796bfbdb711cacae8d5e43885ec4ed
Ruipeng Jia <352111644@qq.com>
2016-10-16 17:32:18 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ##Description#
-	jPainter is a brower-based painting tool,based on HTML5 Canvas and jquery. 
+	jPainter is a brower-based painting tool,based on HTML5 Three.js and jquery.
 ## How to Run it#
    Just open jPainter.html by Internet Explorer,Firfox and etc.
 
-Have fun !!!
+## 声明
+图形学课程设计的作业......
+很抱歉, 烂尾了, 以后有时间再说吧
+./js/line.js  # 这里面的 j_moveLine() 函数中的画的线随鼠标移动的效果一只调试不出来, 还有其它的事情, 先放放
+无限期推迟
+
--- js/line.js
+++ js/line.js
@@ -3,7 +3,7 @@
 //================================================================
 var mat = new THREE.LineBasicMaterial({color: 0xff0000});
 var geo = new THREE.Geometry();
-var line;
+var jline;
 var j_firstPoint = 0;
 function j_drawLine(object, e){
   var ansx = getMousePos(e).x;
@@ -12,12 +12,12 @@ function j_drawLine(object, e){
     new THREE.Vector3(ansx, ansy, 0)
   );
   if(j_firstPoint == 1){
-    line = new THREE.Line(geo, mat);
-    scene.add(line);
-    //line.geometry.vertices[0].x -= 100;
-    geo = new THREE.Geometry();
+    jline = new THREE.Line(geo, mat);
+    scene.add(jline);
+    //jline.geometry.vertices[0].x -= 100;
+    //geo = new THREE.Geometry();
     j_firstPoint = 0;
-    //console.log(line.geometry.vertices[0].x);
+    //console.log(jline.geometry.vertices[0].x);
   } else {
     j_firstPoint = 1;
   }
@@ -26,17 +26,21 @@ $("#canvas-frame").click(j_drawLine)  // 添加鼠标点击监控事件
 $("#canvas-frame").mousemove(j_moveLine);
 
 //================================================================
-// 这里开始移动线
+// 这里开始移动线, 写到这里的移动的就写不下去了
 //================================================================
 function j_moveLine(object, e) {
   event.preventDefault();
-  if(line == null) return;  // 如果为空, 返回
+  if(jline == null) return;  // 如果为空, 返回
   var ansx = getMousePos(e).x;
   var ansy = getMousePos(e).y;
   var mv = new THREE.Vector3(ansx, ansy, 0);
-  scene.remove(line);
-  line.geometry.vertices[0].copy(mv);
-  //line.geometry.vertices[0].x -= 1;
-  console.log(line.geometry.vertices[0].x);
+  scene.remove(jline);
+  jline.geometry.vertices[0].copy(mv);
+  jline.geometry.vertices[0].x -= 1;
+  console.log(jline.geometry.vertices[0].x);
+  geo.vertices.push(new THREE.Vector3(ansx, ansy, 0));
+  geo.vertices.push(new THREE.Vector3(ansx + 100, ansy + 100, 0));
+  //jline = new THREE.Line(jline.geometry, mat);
+  var line = new THREE.Line(geo, mat);
   scene.add(line);
 }
